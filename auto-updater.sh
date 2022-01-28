ffa21e52e5ff975c384f6f07b36a02d0edcbca06
Ranadeep Biswas <mail@rnbguy.at>
2022-01-28 02:38:05 +0100

    conditional CI setup

--- auto-updater.sh
+++ auto-updater.sh
@@ -2,25 +2,30 @@
 
 set -eu
 
-[[ -z "${SSH_PRIV_KEY}" ]] && >&2 echo "no ssh private key" && exit
-[[ -z "${ACTOR}" ]] && >&2 echo "no actor" && exit
+PASUDO=""
 
-# install git, jq, openssh (ssh), pacman-contrib (updpkgsums)
-2>&1 pacman -Syu git jq openssh pacman-contrib --asdeps --needed --noconfirm > /dev/null
+if [[ -n "${CI-}" ]]; then
+    [[ -z "${SSH_PRIV_KEY}" ]] && >&2 echo "no ssh private key" && exit
+    [[ -z "${ACTOR}" ]] && >&2 echo "no actor" && exit
 
-git config --global user.name "${ACTOR}"
-git config --global user.email "ci@github"
+    # install git, jq, openssh (ssh), pacman-contrib (updpkgsums)
+    2>&1 pacman -Syu git jq openssh pacman-contrib --asdeps --needed --noconfirm > /dev/null
 
-# create non-sudo user
-useradd --system pasudo
-echo 'aur ALL=(ALL) NOPASSWD: ALL' > /etc/sudoers.d/aur
+    git config --global user.name "${ACTOR}"
+    git config --global user.email "ci@github"
 
-# setup ssh
-mkdir -p ~/.ssh
-(umask 0077; echo "${SSH_PRIV_KEY}" > ~/.ssh/aur 2> /dev/null)
-(umask 0077; ssh-keyscan aur.archlinux.org >> ~/.ssh/known_hosts 2> /dev/null)
-eval $(ssh-agent) > /dev/null
-2>&1 ssh-add ~/.ssh/aur > /dev/null
+    # create non-sudo user
+    useradd --system pasudo
+    echo 'aur ALL=(ALL) NOPASSWD: ALL' > /etc/sudoers.d/aur
+    PASUDO="sudo -u pasudo"
+
+    # setup ssh
+    mkdir -p ~/.ssh
+    (umask 0077; echo "${SSH_PRIV_KEY}" > ~/.ssh/aur 2> /dev/null)
+    (umask 0077; ssh-keyscan aur.archlinux.org >> ~/.ssh/known_hosts 2> /dev/null)
+    eval $(ssh-agent) > /dev/null
+    2>&1 ssh-add ~/.ssh/aur > /dev/null
+fi
 
 function latest_release() {
     curl -s "https://api.github.com/repos/${1}/releases/latest" \
@@ -45,8 +50,8 @@ ssh aur@aur.archlinux.org list-repos | while read pkgname; do
             chown -R pasudo "${pkgname}"
             cd "${pkgname}"
             sed -i "s/pkgver=.*$/pkgver=${new_ver}/g" PKGBUILD
-            sudo -u pasudo updpkgsums
-            sudo -u pasudo makepkg --printsrcinfo > .SRCINFO
+            ${PASUDO} updpkgsums
+            ${PASUDO} makepkg --printsrcinfo > .SRCINFO
             git commit -am "$new_ver" && git push && echo "[${pkgname}] updated to ${new_ver}"
             git clean -fdx
             cd - > /dev/null
